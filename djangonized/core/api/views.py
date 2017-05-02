7383d2d8e5e93d361530c4d407a8fcb157e8a0eb
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-02 08:58:23 -0500

    Permisos parar el propietario de un Todo

--- djangonized/core/api/views.py
+++ djangonized/core/api/views.py
@@ -1,11 +1,10 @@
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404
-from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from core.models import Todo
+from .permissions import IsOwnerPermission
 from .serializers import (
     UserSerializer,
     TodoSerializer
@@ -20,6 +19,7 @@ class UserViewSet(ModelViewSet):
 class TodoViewSet(ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
+    permission_classes = (IsOwnerPermission,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
--- djangonized/djangonized/settings.py
+++ djangonized/djangonized/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'core',
     'core.api',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
