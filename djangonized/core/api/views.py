d7921d5ab7d48ca561f784c7738ea8f83d40a3ad
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-02 17:17:40 -0500

    Agregar permisos parar el propietario de un Todo

--- djangonized/core/api/views.py
+++ djangonized/core/api/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import status
+from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from .permissions import IsOwnerPermissions
 from core.models import Todo
-from .permissions import IsOwnerPermission
 from .serializers import (
     UserSerializer,
     TodoSerializer
@@ -14,12 +15,13 @@ from .serializers import (
 class UserViewSet(ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = (DjangoModelPermissions,)
 
 
 class TodoViewSet(ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
-    permission_classes = (IsOwnerPermission,)
+    permission_classes = (IsOwnerPermissions,)
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
--- djangonized/djangonized/urls.py
+++ djangonized/djangonized/urls.py
@@ -21,5 +21,6 @@ from rest_framework.authtoken import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('core.api.urls')),
+    url(r'^api-auth', include('rest_framework.urls')),
     url(r'^get_auth_token/', views.obtain_auth_token, name='get_auth_token'),
-]
+ ]
