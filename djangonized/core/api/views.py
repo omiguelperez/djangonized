e2b3daf74e9f1739e48916e89ec0e0c1a1cd7da3
Oscar Pérez <mr.omiguelperez@gmail.com>
2017-05-05 01:35:30 -0500

    Agregar OrderingFilter para facilitar ordenamiento
    - Se orden los Todos por `done`

--- djangonized/core/api/views.py
+++ djangonized/core/api/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import status
-from rest_framework.filters import DjangoFilterBackend, SearchFilter
+from rest_framework.filters import DjangoFilterBackend, SearchFilter, OrderingFilter
 from rest_framework.permissions import DjangoModelPermissions
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -24,9 +24,10 @@ class TodoViewSet(ModelViewSet):
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
     permission_classes = (IsOwnerPermissions,)
-    filter_backends = (DjangoFilterBackend, SearchFilter,)
+    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
     filter_class = TodoFilter
     search_fields = ('=owner__username', 'owner__email')
+    ordering_fields = ('done',)
 
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
